4ec149d76c9d4cef10acee07076979461bf6cedb
Yuriko-Asakura <isip_a.v.plahova@mpt.ru>
2023-03-08 18:14:58 +0300

    player

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -28,14 +28,12 @@ namespace WpfApp2
         private MediaPlayer mediaPlayer = new MediaPlayer();
         bool loadedPlaylist = false;
         int trackIndex, songDuration;
+        bool ag;
         public MainWindow()
         {
             InitializeComponent();
             try
             {
-
-                ButtonPlay.Visibility = Visibility.Visible;
-                ButtonPause.Visibility = Visibility.Hidden;
             }
             catch (FileNotFoundException e)
             {
@@ -60,8 +58,8 @@ namespace WpfApp2
             }
             else
             {
-                ButtonNext.Opacity = 0.5;        //sets button transparency due to end of the playlist
-                ButtonNext.IsEnabled = false;
+                mediaPlayer.Open(new Uri(paths[0]));
+                mediaPlayer.Play();
             }
         }
 
@@ -77,62 +75,39 @@ namespace WpfApp2
         }
 
         public void selectSongs()
-        {             //puts the chosen songs to the playlistListBox
-
+        { 
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic); // "C:\\Users\\admin\\Music";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // "C:\\Users\\admin\\Music";
             openFileDialog.Filter = "music files (*.wav)|*.wav|music files (*.mp3)|*.mp3|All files (*.*)|*.*";
             openFileDialog.FilterIndex = 2;
             openFileDialog.RestoreDirectory = true;
-
             openFileDialog.Multiselect = true;
             if (openFileDialog.ShowDialog() == true)
             {
-                try
-                {
-                    files = openFileDialog.SafeFileNames;    //saves the track names in the tracks array
-                    paths = openFileDialog.FileNames;        //saves the path tracks in the tracks array
+                    files = openFileDialog.SafeFileNames;  
+                    paths = openFileDialog.FileNames;      
                     mediaPlayer.Open(new Uri(paths[0]));
                     StartMusic();
                     for (int i = 0; i < files.Length; i++)
                         PlaylistListBox.Items.Add(files[i]);
                     loadedPlaylist = true;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: Could not read the file from disk. Original error: " + ex.Message);
-                }
             }
         }
         private void ButtonPrevious_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            try
-            {
                 if (trackIndex > 0)
                     trackIndex -= 1;
 
                 mediaPlayer.Open(new Uri(paths[trackIndex]));
                 mediaPlayer.Play();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
         }
 
-        private void ButtonBackwards_MouseDown(object sender, MouseButtonEventArgs e)
-        { //sets 10 seconds rewinds in the media and video players
-            TimeSpan ts = new TimeSpan(0, 0, 0, (int)(mediaPlayer.Position.Seconds - 10));
-            mediaPlayer.Position = ts;
-        }
 
         public void ButtonPause_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (loadedPlaylist == true)
             {
                 mediaPlayer.Pause();
-                ButtonPlay.Visibility = Visibility.Visible;
-                ButtonPause.Visibility = Visibility.Hidden;
             }
         }
 
@@ -141,8 +116,6 @@ namespace WpfApp2
             if (loadedPlaylist == true)
             {
                 mediaPlayer.Play();
-                ButtonPlay.Visibility = Visibility.Hidden;
-                ButtonPause.Visibility = Visibility.Visible;
             }
             else MessageBox.Show("You can add some songs obviously :)");
         }
@@ -171,9 +144,7 @@ namespace WpfApp2
         }
 
         private void timer_Tick(object sender, EventArgs e)
-        {       //
-            try
-            {
+        {      
                 if ((mediaPlayer.Source != null) && (mediaPlayer.NaturalDuration.HasTimeSpan))
                 {
                     MediaPlayerSlider.Minimum = 0;
@@ -181,26 +152,13 @@ namespace WpfApp2
                     MediaPlayerSlider.Value = mediaPlayer.Position.TotalSeconds;
                     songDuration = (int)(mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds);
                     TextBlockSongDuration.Text = TimeSpan.FromSeconds(songDuration).ToString(@"m\:ss");
-
                     if (MediaPlayerSlider.Maximum == MediaPlayerSlider.Value) //if end of the track the media player plays the next track
                         playNextTrack();
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
         private void PlaylistListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {  //plays the selected track
-            try
-            {
+        { 
                 StartMusic();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
 
         private void StartMusic()
@@ -209,24 +167,15 @@ namespace WpfApp2
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
-
             mediaPlayer.Play();
-            ButtonPlay.Visibility = Visibility.Hidden;
-            ButtonPause.Visibility = Visibility.Visible;
             trackIndex = PlaylistListBox.SelectedIndex;
         }
 
         private void PlaylistListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             mediaPlayer.Open(new Uri(paths[PlaylistListBox.SelectedIndex]));
-            try
-            {
+            
                 StartMusic();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
 
         private void PlaylistListBox_Drop(object sender, DragEventArgs e)
@@ -236,35 +185,20 @@ namespace WpfApp2
             selectSongs();
         }
 
-        private void MenuItemEyeKeeper_Click(object sender, RoutedEventArgs e)
-        {
-        }
         private void Again1_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (!ag)
             {
-                mediaPlayer.Open(new Uri(paths[trackIndex]));
-                mediaPlayer.Play();
+                Again1.BorderBrush = Brushes.MediumVioletRed;
+                ag = true;
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.ToString());
+                Again1.BorderBrush = Brushes.White;
+                ag = false;
             }
         }
-        private void stop_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-
-        private void ButtonPlay_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            Environment.Exit(0);
-        }
-    
+        private void ButtonPlay_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e){}
     }
 }
